c3dd0f4ec61a799f1a53236cc71b1461b9e3104c
Ratan Othayoth <ratan.othayoth@gmail.com>
2025-03-12 10:37:52 -0400

    input command uses light ID instead of PIN number

--- spotlight/spotlight.ino/spotlight.ino.ino
+++ spotlight/spotlight.ino/spotlight.ino.ino
@@ -1,5 +1,7 @@
 const unsigned int N_LIGHTS = 11;  
+const unsigned int LIGHT_IDS[N_LIGHTS] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
 const unsigned int LIGHT_PINS[N_LIGHTS] = {2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+const int ID_2_PIN_OFFSET = 1; // PIN = ID + ID_2_PIN_OFFSET
 bool L_STATES[N_LIGHTS] = {LOW};  
 
 void setup() {
@@ -8,26 +10,31 @@ void setup() {
         pinMode(LIGHT_PINS[i], OUTPUT);
         digitalWrite(LIGHT_PINS[i], L_STATES[i]);
     }
-    Serial.println("Send space-separated pin numbers followed by 'T', 'ON', or 'OFF'.");
+    Serial.println("Send space-separated light IDS followed by 'T', 'ON', or 'OFF'.");
 }
+
 
 void loop() {
     if (Serial.available()) {
+
+        // read a full command
         String input = Serial.readStringUntil('\n');  // Read full input line
         input.trim();  // Remove whitespace
         processCommand(input);
     }
 }
 
+
+// process the input command
 void processCommand(String input) {
-    int pinsToToggle[N_LIGHTS] = {0};  
+    unsigned int pinsToToggle[N_LIGHTS] = {0};  
     int count = 0;
     String command = "";
 
     char *ptr = strtok((char *)input.c_str(), " ");
     while (ptr != NULL) {
         if (isDigit(ptr[0])) {  
-            int pin = atoi(ptr);
+            int pin = atoi(ptr) + ID_2_PIN_OFFSET;
             for (int i = 0; i < N_LIGHTS; i++) {
                 if (LIGHT_PINS[i] == pin) {
                     pinsToToggle[count++] = i;
